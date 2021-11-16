80faab33fdbe7ccd36c94122f5504a0fbc631e40
AntonioAiltonGocalves <antonioailton723@gmail.com>
2021-11-16 10:22:51 -0300

    feature: start kibelicia

---
+++ js/semaforo/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const timeCar = document.querySelector('timeCar');
+const timePerson = document.querySelector('timePerson');
+
+const greenCar = document.getElementById('greenCar');
+const redCar = document.getElementById('redCar');
+const yellowCar = document.getElementById('yellowCar');
+
+function setGreenForCar () {
+    document.getElementsByTagName('head')[0].getElementById('stylesheet').href='css/';
+}
