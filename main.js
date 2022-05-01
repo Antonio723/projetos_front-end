ca90cc417a59b8adffb7dab267eebb57ecc2afda
Antonio Ailton Gonçalves <62522868+Antonio723@users.noreply.github.com>
2022-05-01 10:45:34 -0300

    USANDO GOOGLE ADS

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-atOptions = {
-    'key' : '8a7c8606a7575ce0f3d0f6bbd4e5d0b7',
-    'format' : 'iframe',
-    'height' : 60,
-    'width' : 468,
-    'params' : {}
-};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/8a7c8606a7575ce0f3d0f6bbd4e5d0b7/invoke.js"></scr' + 'ipt>');
+// atOptions = {
+//     'key' : '8a7c8606a7575ce0f3d0f6bbd4e5d0b7',
+//     'format' : 'iframe',
+//     'height' : 60,
+//     'width' : 468,
+//     'params' : {}
+// };
+// document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/8a7c8606a7575ce0f3d0f6bbd4e5d0b7/invoke.js"></scr' + 'ipt>');
+
