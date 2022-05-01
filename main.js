39b0a75ce46e61824879c47d915d12872a98dff9
Antonio Ailton Gonçalves <62522868+Antonio723@users.noreply.github.com>
2022-05-01 07:36:51 -0300

    anuncio em arquivo a parte

---
+++ main.js
@@ -0,0 +1,8 @@
+atOptions = {
+    'key' : '8a7c8606a7575ce0f3d0f6bbd4e5d0b7',
+    'format' : 'iframe',
+    'height' : 60,
+    'width' : 468,
+    'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/8a7c8606a7575ce0f3d0f6bbd4e5d0b7/invoke.js"></scr' + 'ipt>');
